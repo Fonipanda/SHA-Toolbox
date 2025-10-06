43551c3cf0e0383927740e56f420d1ca096975c5
Fonipanda <francois.soulie@outlook.com>
2025-10-06 14:22:49 +0200

    Update main.py

--- roles/ips_toolbox_controlm/defaults/main.py
+++ roles/ips_toolbox_controlm/defaults/main.py
@@ -1,1 +1,11 @@
+---
+
+
+controlm_component: ""
+controlm_operation: ""
+controlm_task_file_name: "{{ controlm_operation }}_{{ ansible_system }}_{{ controlm_component }}.yml"
+controlm_script_mode: false
+controlm_result_name: ""
+controlm_agent_name: "Default"
+controlm_agentaccount: "ctmagent" 
 
