e53f169406d9514ca86c6e40a62c5040e0c21b2b
Fonipanda <francois.soulie@outlook.com>
2025-10-06 15:05:42 +0200

    Create main.py

---
+++ roles/ips_toolbox_illumio/defaults/main.py
@@ -0,0 +1,9 @@
+---
+
+
+illumio_component: ""
+illumio_operation: ""
+illumio_task_file_name: "{{ illumio_operation }}_{{ ansible_system }}_{{ illumio_component }}.yml"
+illumio_script_mode: false
+illumio_result_name: ""
+
