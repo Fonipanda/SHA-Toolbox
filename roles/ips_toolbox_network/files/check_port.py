d9569d12191e05101706ef4aeda9e88bbf31b5ab
Fonipanda <francois.soulie@outlook.com>
2025-10-06 16:23:52 +0200

    Create check_port.py

---
+++ roles/ips_toolbox_network/files/check_port.py
@@ -0,0 +1,19 @@
+import socket
+import sys
+
+def isopen(hostname, port, milliseconds):
+  seconds=(int(milliseconds)/1000)%60
+  try:
+      sock = socket.create_connection((hostname, port), timeout=int(seconds))
+      print "connection succeeded"
+  except socket.timeout as err:
+      print err
+  except socket.error as err:
+      print err
+
+if __name__ == "__main__":
+  try:
+    hostname, port, milliseconds = sys.argv[1:]
+    isopen(hostname, port, milliseconds)
+  except ValueError:
+    print "Not enough arguments"
