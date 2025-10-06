4507dd67e10d339784fce9221cab0509ffc89b91
Fonipanda <francois.soulie@outlook.com>
2025-10-06 19:52:01 +0200

    Update main.py

--- roles/ips_toolbox_services/defaults/main.py
+++ roles/ips_toolbox_services/defaults/main.py
@@ -1,1 +1,9 @@
+---
+
+ services_component: ""
+ services_operation: ""
+ services_task_file_name: "{{ services_operation }}_{{ ansible_system }}_{{ services_component }}.yml"
+ services_script_mode: false
+ services_result_name: "result"
+
 
