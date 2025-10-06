e144e425c7f8de82e91252425be4ab81db49e90c
Fonipanda <francois.soulie@outlook.com>
2025-10-06 22:18:18 +0200

    Update main.py

--- roles/ips_toolbox_sqlserver/defaults/main.py
+++ roles/ips_toolbox_sqlserver/defaults/main.py
@@ -1,1 +1,10 @@
+---
+
+
+sqlserver_component: ""
+sqlserver_operation: ""
+sqlserver_task_file_name: "{{ sqlserver_operation }}_{{ ansible_system }}_{{ sqlserver_component }}.yml"
+sqlserver_script_mode: false
+sqlserver_result_name: ""
+
 
