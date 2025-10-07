7caaecbabf81c5acfaf9d0fa0b45f207b57c395d
Fonipanda <francois.soulie@outlook.com>
2025-10-07 11:31:39 +0200

    Create main.py

---
+++ roles/ips_toolbox_webserver/defaults/main.py
@@ -0,0 +1,18 @@
+---
+
+
+webserver_component: ""
+webserver_operation: ""
+webserver_task_file_name: "{{ webserver_operation }}_{{ ansible_system }}_{{ webserver_component }}.yml"
+webserver_script_mode: false
+webserver_result_name: ""
+webserver_ref: ""
+webserver_item: ""
+webserver_url: ""
+webserver_kdb_pwd: ""
+webserver_ou: ""
+webserver_force: "no"
+webserver_kdb_path: ""
+webserver_crt_file: ""
+webserver_grp_type: ""
+
